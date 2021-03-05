7f586ed21b9e2971a1e7501ecde5bf4df69f4571
Félix Chenette-Stewart <felix@nexumnet.com>
2021-03-05 11:40:55 -0500

    updated layout, added kinova colors to theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+const KinovaColor =  Color(0xFF00147A);
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar:AppBar(
+      title: Text('Restaurant Kinova'),
+      centerTitle: true,
+      backgroundColor: KinovaColor,
+    ),
+  ),
+));
